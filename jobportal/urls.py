47914dd30e664e25ed8ffdaad3ca05e91baddc50
nikitakumari0507 <nikitakumari20060206@gmail.com>
2026-08-24 20:05:54 +0530

    Fix company logos on Render and remove private media

--- jobportal/urls.py
+++ jobportal/urls.py
@@ -1,22 +1,38 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path(
+        "admin/",
+        admin.site.urls
+    ),
 
-    path("", include("core.urls")),
+    path(
+        "",
+        include("core.urls")
+    ),
 
-    path("accounts/", include("accounts.urls")),
+    path(
+        "accounts/",
+        include("accounts.urls")
+    ),
 
-    path("jobs/", include("jobs.urls")),
+    path(
+        "jobs/",
+        include("jobs.urls")
+    ),
+
+    # Serve only public company logos
+    re_path(
+        r"^media/company_logos/(?P<path>.*)$",
+        serve,
+        {
+            "document_root": (
+                settings.MEDIA_ROOT / "company_logos"
+            )
+        },
+    ),
 ]
-
-
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
